117e3596481bc19e60361d1e91a55203921a4ab4
Brett Clifton <bclifton@adadapted.com>
2023-03-30 12:07:11 -0400

    feat: update podspec

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "AAMultiplatformLib",
             url: "https://github.com/adadaptedinc/AAMultiplatformLib/AAMultiplatformLib.zip",
-            checksum: "e6bebcddc59405b839811c349271ffea7ccb43ae4f0b61257f7a7e8bd9089934"
+            checksum: "ceed3270c389e41db7f83f22a97479a59d52c5c2ba0bfe6b10902de50af93b0d"
         ),
     ]
 )
